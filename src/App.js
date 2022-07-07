8916ad76b49ee16ea925ba9fbb0e6585dbbed588
Antonina <macbookpro@MacBook-Pro-Macbook.local>
2022-07-07 14:05:56 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -22,28 +22,28 @@ const App = () => {
   }, [])
 
 
-  // const posts = [
-  //   { id: "1", name: "user1" },
-  //   { id: "2", name: "user2" },
-  //   { id: "3", name: "user3" },
-  //   { id: "4", name: "user4" },
-  //   { id: "5", name: "user5" },
-  //   { id: "6", name: "user6" },
-  //   { id: "7", name: "user7" },
-  //   { id: "8", name: "user8" },
-  //   { id: "9", name: "user9" },
-  //   { id: "10", name: "user10" },
-  //   { id: "11", name: "user11" },
-  //   { id: "12", name: "user12" },
-  //   { id: "13", name: "user13" },
-  //   { id: "14", name: "user14" },
-  //   { id: "15", name: "user15" },
-  //   { id: "16", name: "user16" },
-  //   { id: "17", name: "user17" },
-  //   { id: "18", name: "user18" },
-  //   { id: "19", name: "user19" },
-  //   { id: "20", name: "user20" },
-  // ];
+  const posts = [
+    { id: "1", name: "user1" },
+    { id: "2", name: "user2" },
+    { id: "3", name: "user3" },
+    { id: "4", name: "user4" },
+    { id: "5", name: "user5" },
+    { id: "6", name: "user6" },
+    { id: "7", name: "user7" },
+    { id: "8", name: "user8" },
+    { id: "9", name: "user9" },
+    { id: "10", name: "user10" },
+    { id: "11", name: "user11" },
+    { id: "12", name: "user12" },
+    { id: "13", name: "user13" },
+    { id: "14", name: "user14" },
+    { id: "15", name: "user15" },
+    { id: "16", name: "user16" },
+    { id: "17", name: "user17" },
+    { id: "18", name: "user18" },
+    { id: "19", name: "user19" },
+    { id: "20", name: "user20" },
+  ];
 
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(4);
@@ -54,6 +54,8 @@ const App = () => {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = repo.slice(indexOfFirstPost, indexOfLastPost);
 
+  const currentPostsObject = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   // when user clicks on number this function will execute
 
   const paginate = (number) => {
@@ -62,13 +64,26 @@ const App = () => {
 
   return (
     <>
-      <h1>Pagination Example</h1>
+      <br />
+      <h1>Pagination Example Not Working</h1>
       <div style={{ width: "300px", textAlign: "center" }}>
         {currentPosts.map((item) => {
           return <p key={item.results.id}>{item.results.id}</p>;
         })}
         <Pagination
           totalPosts={repo.length}
+          postsPerPage={postsPerPage}
+          paginate={paginate}
+        />
+      </div>
+      <br />
+      <h1>Pagination Example Workig</h1>
+      <div style={{ width: "300px", textAlign: "center" }}>
+        {currentPostsObject.map((item) => {
+          return <p key={item.id}>{item.name}</p>;
+        })}
+        <Pagination
+          totalPosts={posts.length}
           postsPerPage={postsPerPage}
           paginate={paginate}
         />
